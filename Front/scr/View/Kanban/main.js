87a53e415975aabe267adc7cd68337d99ec599a8
VictorM4tos <108440162+VictorM4tos@users.noreply.github.com>
2022-10-23 15:00:04 -0300

    ajuste css

--- Front/scr/View/Kanban/main.js
+++ Front/scr/View/Kanban/main.js
@@ -3,7 +3,8 @@ function log(message) {
   console.log(">" + message);
 }
 
-const cards = document.querySelectorAll("card");
+// Test Drag and drop
+const cards = document.querySelectorAll(".card");
 
 cards.forEach((card) => {
   card.addEventListener("dragstar", dragstar);
@@ -12,13 +13,39 @@ cards.forEach((card) => {
 });
 
 function dragstar() {
-  log("CARD: Start Dragging");
+  //   log("CARD: Start Dragging");
 }
 
 function drag() {
-  log("CARD: Is Dragging");
+  //   log("CARD: Is Dragging");
 }
 
 function dragend() {
-  log("CARD: Stop Dragging");
+  //   log("CARD: Stop Dragging");
+}
+
+// place where we will drop cards
+
+const tarefa = document.querySelectorAll(".kanban__dropzone");
+
+tarefa.forEach((kanban__dropzone) => {
+  kanban__dropzone.addEventListener("dragInitt", drageventer);
+  kanban__dropzone.addEventListener("dragOver", dragover);
+  kanban__dropzone.addEventListener("dragLeave", dragleave);
+  kanban__dropzone.addEventListener("drop", drop);
+});
+
+function drageventer() {
+  log("DROPZONE: Drag Initt");
+}
+
+function dragover() {
+  log("DROPZONE:Drag Over");
+}
+
+function dragleave() {
+  log("DROPZONE:Drag Leave");
+}
+function drop() {
+  log("DROPZONE: drop");
 }
