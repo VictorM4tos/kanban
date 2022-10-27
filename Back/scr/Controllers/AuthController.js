4e41e92912e61ddb8a6771a65cdbf697c6e16281
Victor Matos <victor.matos@Vector.com>
2022-10-27 18:03:37 -0300

    ajuste

--- Back/scr/Controllers/AuthController.js
+++ Back/scr/Controllers/AuthController.js
@@ -1,16 +1,21 @@
 const UserSchema = require("../models/userSchema");
 const bcrypt = require("bcrypt");
 
-const login = async (req, res) => {
+const loginUser = async (req, res) => {
   try {
-    const findUser = req.body.login;
-    const userlocalizado = await UserSchema.findById(findUser);
+    const findUser = await UserSchema.findById(req.body.email);
+    const validPassword = bcrypt.compareSync(req.body.senha, user.password);
     res.status(201).send({
       message: "Usuário localizado",
-      userlocalizado,
+      email: `${req.body.email}`,
     });
   } catch (e) {
-    console.error(e);
+    console.log(
+      res.status(401).send({
+        message: "Usuário Não localizado",
+        email: `${req.body.email}`,
+      })
+    );
   }
 };
 
--- Back/scr/routes/UserRoutes.js
+++ Back/scr/routes/UserRoutes.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 const UserController = require("../Controllers/UserController");
-const { route } = require("./TarefasRoutes");
+const AuthController = require("../Controllers/AuthController");
 // console.log(controllers);
 
 // Routes UserController
@@ -13,6 +13,6 @@ router.get("/:id", UserController.findId);
 router.post("/create", UserController.createusers);
 router.patch("/:id", UserController.updateUser);
 router.delete("/:id", UserController.deleteUser);
-router.login("/login", UserController, loginUser);
+router.post("/login", AuthController.loginUser);
 
 module.exports = router;
